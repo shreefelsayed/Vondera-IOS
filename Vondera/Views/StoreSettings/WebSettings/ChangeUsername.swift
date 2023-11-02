542d7e0cbdce38d8ac6d3baba6e0d0f6131d2035
Shreif El Sayed <abdulrhman5640@a-arts.helwan.edu.eg>
2023-11-02 12:54:40 +0200

    A lot of fixes and updates

---
+++ Vondera/Views/StoreSettings/WebSettings/ChangeUsername.swift
@@ -0,0 +1,18 @@
+//
+//  ChangeUsername.swift
+//  Vondera
+//
+//  Created by Shreif El Sayed on 01/11/2023.
+//
+
+import SwiftUI
+
+struct ChangeUsername: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChangeUsername()
+}
